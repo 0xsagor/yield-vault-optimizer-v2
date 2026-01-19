e522e9b65a08fa0f8ed0ffd8312d9f1cc20a8ee6
Sagor <161376518+0xsagor@users.noreply.github.com>
2026-01-19 13:46:19 +0600

    Create Interfaces.sol

---
+++ Interfaces.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
+}
+
+interface IStrategy {
+    function deposit() external;
+    function withdraw(uint256 amount) external;
+    function balanceOf() external view returns (uint256);
+}
+
+interface IMasterChef {
+    function deposit(uint256 _pid, uint256 _amount) external;
+    function withdraw(uint256 _pid, uint256 _amount) external;
+    function userInfo(uint256 _pid, address _user) external view returns (uint256, uint256);
+}
